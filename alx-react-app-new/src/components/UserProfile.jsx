a1901f1d6af5dd4f4ac6b65f61204d6abb3da103
Magongoa Moses <magongoamoses@gmail.com>
2025-11-09 21:27:58 +0200

    update react components with new styling

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,8 +1,8 @@
 const UserProfile = (props) => {
     return (
-        <div style={{ fontFamily: 'sans-serif', color: 'lightblue', border: '1px solid #ccc', borderRadius: '8px', padding: '15px', margin: '20px auto', maxWidth: '400px' }}>
+        <div style={{ fontFamily: 'sans-serif', color: 'lightblue', border: '1px solid #ccc', borderRadius: '10px', padding: '15px', margin: '20px auto', maxWidth: '400px' }}>
             <h2 style={{ fontSize: '24px', fontWeight: 'bold' }}>{props.name}</h2>
-            <p>Age: {props.age}</p>
+            <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
             <p>Bio: {props.bio}</p>
         </div>
     );
