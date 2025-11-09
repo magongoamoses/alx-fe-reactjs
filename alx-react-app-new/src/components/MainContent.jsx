f8e008482114f44e6e4eb07ca10dfcc9f9c55eeb
Magongoa Moses <magongoamoses@gmail.com>
2025-11-09 21:29:58 +0200

    update react components with new styling

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,7 +2,7 @@ function MainContent() {
     return (
         <div style={{ fontFamily: 'sans-serif', color: 'lightblue', margin: '20px 0' }}>
             <main>
-                <p>I love to visit <span style={{ fontWeight: 'bold' }}>New York, Paris, and Tokyo.</span></p>
+                <p>I love to visit New York, Paris, and Tokyo.</p>
             </main>
         </div>
     );
