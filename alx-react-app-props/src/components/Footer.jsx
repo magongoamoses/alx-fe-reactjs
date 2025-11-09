82b7bcd971c78629b47907230b1360de62982e85
Magongoa Moses <magongoamoses@gmail.com>
2025-11-09 23:13:24 +0200

    Implement Context API and remove props

---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <div style={{ fontFamily: 'sans-serif', color: 'white', padding: '10px', marginTop: '20px' }}>
+            <footer>
+                <p>© 2023 City Lovers</p>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
