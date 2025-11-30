3dcaf17b8134042bb12d704d4a7f7282d452c5f8
Magongoa Moses <magongoamoses@gmail.com>
2025-11-30 18:29:07 +0200

    fix errors in UserProfile component

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile() {
     return (
         <div className="bg-gray-100 max-w-xs mx-auto my-20 rounded-lg shadow-lg md:max-w-sm sm:p-4 md:p-8">
-            <img src="https://via.placeholder.com/150" alt="User" className="rounded-full mx-auto w-24 h-24 md:w-36 md:h-36" />
+            <img src="https://via.placeholder.com/150" alt="User" className="rounded-full mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36" />
             <h1 className="text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
             <p className="text-gray-600 text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
         </div>
