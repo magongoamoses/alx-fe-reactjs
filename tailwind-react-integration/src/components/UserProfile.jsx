de7c510f32c5878d99884fbecebce289bb1c55e4
Magongoa Moses <magongoamoses@gmail.com>
2025-11-30 18:27:07 +0200

    update UserProfile component for responsiveness for small screens

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
     return (
-        <div className="bg-gray-100 max-w-xs mx-auto my-20 rounded-lg shadow-lg md:max-w-sm p-4 md:p-8">
+        <div className="bg-gray-100 max-w-xs mx-auto my-20 rounded-lg shadow-lg md:max-w-sm sm:p-4 md:p-8">
             <img src="https://via.placeholder.com/150" alt="User" className="rounded-full mx-auto w-24 h-24 md:w-36 md:h-36" />
             <h1 className="text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
             <p className="text-gray-600 text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
