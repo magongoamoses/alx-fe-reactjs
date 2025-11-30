14f2907088e0ce2241f1d2df4326de82db83a706
Magongoa Moses <magongoamoses@gmail.com>
2025-11-30 17:17:55 +0200

    style the UserProfile component

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
 import './App.css'
+import UserProfile from './components/UserProfile'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <h1>Welcome to Tailwind CSS</h1>
-      </div>
-    </>
+    <div>
+      <UserProfile />
+    </div>
+
   )
 }
 
