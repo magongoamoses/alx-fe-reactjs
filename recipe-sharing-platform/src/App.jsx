60b1eb2b45098079867343d425f9c28f1d8dcb92
Magongoa Moses <magongoamoses@gmail.com>
2025-12-06 16:27:26 +0200

    create new project and setup tailwind css

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <h1 className='text-blue-500'>Welcome to the react app</h1>
+        </>
+    )
+}
+
+export default App
